b220aa916cdb9e409b45b82a0d29566eab0a6cd8
coihsan <110887888+coihsan@users.noreply.github.com>
2023-12-01 09:53:08 +0000

    Update 4 files

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,17 @@
+import { IconList } from "../constants/Icons.tsx";
+import Link from 'next/link'
+import Image from 'next/image'
 export default function Footer() {
   return (
     <footer className="styleSection">
-      <div></div>
+      <div>
+        {IconList.map((icon) =>(
+          <link key={icon.id} href={icon.url}>
+            <span className="">{icon.svg}</span>
+            <span className="hidden">{icon.title}</span>
+          </Link>
+        ))}
+      </div>
     </footer>
   );
 }
