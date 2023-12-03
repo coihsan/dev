625a75d262c7710bb90fac1d520d75adc0e4c8fb
coihsan <110887888+coihsan@users.noreply.github.com>
2023-12-03 07:35:38 +0000

    Update 6 files

--- app/UI/ReadMoreOverlay.tsx
+++ app/UI/ReadMoreOverlay.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 export default function ReadMoreOverlay() {
   return (
     <div className="flex justify-center items-end mx-auto absolute bottom-0 w-full h-[20%] bg-gradient-to-t from-[#0b0b0b] to-transparent">
-      <div>
+      <div className="">
         <Link
           className="px-4 py-2 rounded-full bg-[#f0f0f0] text-[#121212] w-max"
-          href={"/projects"}
+          href={"/project"}
         >
           Index
         </Link>
