b0114854bc70c519417b90ba4298c5d1a5387024
coihsan <110887888+coihsan@users.noreply.github.com>
2023-12-04 09:16:42 +0000

    Update Cookies.tsx

--- app/UI/Cookies.tsx
+++ app/UI/Cookies.tsx
@@ -23,20 +23,23 @@ const Cookies = () => {
   // }
 
   return (
-    <div className="z-50 mx-auto fixed bottom-5 left-5 max-[600px]:left-0 max-[600px]:bottom-0 flex justify-center  w-[30%] max-[600px]:w-full backdrop-blur-lg text-neutral-50 border border-neutral-500 rounded-lg">
-      <div className="grid  items-center justify-between gap-5 max-[600px]:flex-col p-5">
+    <motion.div
+      className="z-50 mx-auto fixed bottom-5 left-5 max-[600px]:left-0 max-[600px]:right-0 max-[600px]:bottom-0 flex justify-center  w-[30%] max-[600px]:w-[90%] backdrop-blur-lg text-neutral-50 border border-neutral-500 rounded-lg"
+      animate={{ opacity: 0 }}
+      transition={{ ease: [0.17, 0.67, 0.83, 0.67] }}
+    >
+      <div className="grid items-center justify-between gap-5 max-[600px]:flex-col p-5 mx-auto">
         <div className="flex items-center gap-2">
           <p className="max-[600px]:text-[14px] max-[600px]:text-center flex max-[600px]:flex-col ">
-            <span className="max-[600px]:text-[4rem]">🍪 </span> This page use
-            cookies to provide you with a better browsing experience and to
-            analyze our traffic.
+            🍪 This site uses cookies to understand your experience and analyze
+            our traffic.
           </p>
         </div>
-        <button className="px-2 py-1 rounded-md bg-neutral-200 max-[600px]:text-[14px] text-neutral-950 font-medium">
+        <button className="px-2 py-1 rounded-md bg-neutral-200 hover:bg-neutral-400 max-[600px]:text-[14px] text-neutral-950 font-medium">
           Okay!
         </button>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
