e0942e1eee70cd31019b0e2ca326458c5e35218f
coihsan <110887888+coihsan@users.noreply.github.com>
2023-12-14 15:19:24 +0000

    Update 3 files

--- app/components/experties.tsx
+++ app/components/experties.tsx
@@ -1,19 +1,15 @@
 "use client";
 import Image from "next/image";
+import { useRef } from "react";
 import { motion, whileInView } from "framer-motion";
+import Animated from "../UI/animate";
 import { skillList, commonjob } from "../constants/data";
 export default function Experties() {
   return (
     <section className="relative bg-[url('/grid.svg')] max-[600px]:bg-[center_top_2rem] max-[600px]:bg-[length:795px_400px] bg-contain bg-no-repeat bg-center w-full max-[600px]:py-[4rem]">
       <div className="styleSection">
         <div className="flex flex-col relative">
-          <motion.div
-            className="flex items-center sideLine"
-            initial={{ opacity: 0, y: 10 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 1 }}
-            viewport={{ once: true }}
-          >
+          <motion.div className="flex items-center sideLine">
             <Image
               className="align-baseline max-[600px]:w-[30px]"
               src={"/terminal.svg"}
@@ -21,25 +17,13 @@ export default function Experties() {
               height={40}
               alt="icon terminal"
             />
-            <motion.h3
-              className="headingStyle align-baseline pl-[1rem]"
-              initial={{ opacity: 0, y: 10 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1.5, delay: 1.5 }}
-              viewport={{ once: true }}
-            >
+            <Animated className="headingStyle align-baseline pl-[1rem]">
               Experties
-            </motion.h3>
+            </Animated>
           </motion.div>
 
           <div className="pl-[3.7rem] max-[600px]:pl-[2rem]">
-            <motion.span
-              className="text-[#E3E3E3] max-w-3xl"
-              initial={{ opacity: 0, y: 10 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1.7, delay: 1.7 }}
-              viewport={{ once: true }}
-            >
+            <motion.span className="text-[#E3E3E3] max-w-3xl">
               I can handle jobs like a{" "}
               <em className="blockText">Digital Marketing</em>,{" "}
               <em className="blockText">Graphic Design</em> and{" "}
@@ -47,22 +31,12 @@ export default function Experties() {
               department with a team or individually.
             </motion.span>
             <div className="pt-[2rem]">
-              <motion.h1
-                className="font-medium text-[1.5rem] pb-[1rem]"
-                initial={{ opacity: 0, y: 10 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1.9, delay: 1.9 }}
-                viewport={{ once: true }}
-              >
+              <motion.h1 className="font-medium text-[1.5rem] pb-[1rem]">
                 Tech Stack
               </motion.h1>
               <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2  max-[600px]:grid-cols-2 gap-[10px] w-full">
                 {skillList.map((item) => (
                   <motion.div
-                    initial={{ opacity: 0, scale: 0.9 }}
-                    whileInView={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 1.8, delay: 1 }}
-                    viewport={{ once: true }}
                     key={item.id}
                     className="boxWrapper p-px group rounded-[15px] transition-colors duration-500 ease-linear "
                   >
@@ -88,10 +62,6 @@ export default function Experties() {
               <div className="flex items-center flex-wrap gap-[10px] w-full">
                 {commonjob.map((items) => (
                   <motion.div
-                    initial={{ opacity: 0, scale: 0.9 }}
-                    whileInView={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 1.8, delay: 1 }}
-                    viewport={{ once: true }}
                     key={items.id}
                     className="boxWrapper group p-px rounded-full"
                   >
